5e6d60a36397cbb38c8e7c7591f6ff1dd43af50a
Diego Nunes Reis <diegonunesftw@gmail.com>
2019-08-06 23:14:39 -0300

    more stuff

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const routes = require('./routes');
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` });
+mongoose.connect('mongodb+srv://omnistack:chatubademesquita@cluster0-kefiy.mongodb.net/omnistack8?retryWrites=true&w=majority', { 
+    useNewUrlParser: true 
 });
-
+server.use(express.json());
+server.use(routes);
 server.listen(3333);
